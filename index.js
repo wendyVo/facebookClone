a2d16675667f690f8498994c21014c04ee59fd94
Wendy Uyen Vo <uyen199247@gmail.com>
2021-09-26 23:08:47 +1000

    initialize projects

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+//Connect to MongoDb
+mongoose.connect(process.env.MONGO_URL, () => {
+    console.log("Connected to MongoDb")
+});
+
+//MiddleWare
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+app.listen(8000, () => {
+    console.log("Backend server is running!")
+})
