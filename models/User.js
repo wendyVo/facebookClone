211888af3c028382cc096b7395c556fcb1bc9c65
Wendy Uyen Vo <uyen199247@gmail.com>
2021-09-28 22:05:33 +1000

    updated users route

--- models/User.js
+++ models/User.js
@@ -31,13 +31,29 @@ const UserSchema = new mongoose.Schema({
         type: Array,
         default: []
     },
-    following: {
+    followings: {
         type: Array,
         default: []
     },
     isAdmin: {
         type: Boolean,
-        default: false,
+        default: false
+    },
+    desc: {
+        type: String,
+        max:50
+    },
+    city: {
+        type: String,
+        max:50
+    },
+    from: {
+        type: String,
+        max:50
+    },
+    relationship: {
+        type: Number,
+        enum:[1,2,3],
     }
 },
     { timestamps: true }
